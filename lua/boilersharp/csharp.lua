3da2fb3b1e8fc1ce72958918c92192a05bb3da64
DestopLine <destopline555@hotmail.com>
2024-08-04 11:24:23 -0400

    Implement usings by version

--- lua/boilersharp/csharp.lua
+++ lua/boilersharp/csharp.lua
@@ -32,6 +32,28 @@ function M.get_namespace()
   end
 end
 
+---@return string?
+function M.get_dotnet_version()
+  local ok, result = pcall(function()
+    return vim.system({ "dotnet", "--version" }, { text = true }):wait()
+  end)
+  if ok then
+    if result.stdout == nil or result.code ~= 0 then
+      print("Boilersharp: dotnet cli returned a bad result")
+      return nil
+    end
+    return vim.trim(result.stdout)
+  end
+  print("Boilersharp: dotnet executable not found")
+  return nil
+end
+
+---@param version string
+---@return number?
+function M.parse_major_dotnet_version(version)
+  return tonumber(string.match(version, "%d+"))
+end
+
 ---@return string
 function M.get_type_name()
   -- The pattern gets the filename until the first dot, as opposed to
@@ -54,14 +76,33 @@ local USINGS = {
 
 ---@return string[]
 function M.get_usings()
+  local use
+
   if cfg.opts.add_usings == "never" then
-    return {}
+    use = true
+
   elseif cfg.opts.add_usings == "always" then
-    return USINGS
-  else
-    --  TODO: Calculate auto usings
-    return {}
+    use = false
+
+  elseif cfg.opts.add_usings == "version" then
+    local version = M.get_dotnet_version()
+    if version == nil then
+      use = true
+    else
+      local major = M.parse_major_dotnet_version(version)
+      if major == nil then
+        print("Boilersharp: unable to parse dotnet version")
+        use = true
+      else
+        use = major < 6
+      end
+    end
+
+  elseif cfg.opts.add_usings == "csproj" then
+    --  TODO: Calculate csproj usings
   end
+
+  if use then return USINGS else return {} end
 end
 
 ---@return boolean
