d3d981e8d3c2ff43f314ac59db3ff0f176e4bd3a
DestopLine <destopline555@hotmail.com>
2024-12-21 14:54:37 -0400

    feat(core)!: move write_boilerplate_if_empty() functionality to write_boilerplate()

--- lua/boilersharp/autocommands.lua
+++ lua/boilersharp/autocommands.lua
@@ -7,21 +7,8 @@ function M.add_autocommands()
         desc = "Write C# boilerplate when entering an empty C# file",
         group = vim.api.nvim_create_augroup("Boilersharp", { clear = true }),
         pattern = "*.cs",
-        callback = function() M.write_boilerplate_if_empty(0) end,
+        callback = function() core.write_boilerplate(0) end,
     })
-end
-
----@param bufnr integer
-function M.write_boilerplate_if_empty(bufnr)
-    if vim.api.nvim_buf_line_count(bufnr) > 1 then
-        return
-    end
-
-    if #vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] > 0 then
-        return
-    end
-
-    core.write_boilerplate(bufnr)
 end
 
 return M
--- lua/boilersharp/core.lua
+++ lua/boilersharp/core.lua
@@ -2,8 +2,24 @@ local boilerplate = require("boilersharp.boilerplate")
 
 local M = {}
 
----@param bufnr integer
-function M.write_boilerplate(bufnr)
+---@param bufnr integer | nil
+---@param ensure_empty boolean | nil
+function M.write_boilerplate(bufnr, ensure_empty)
+    bufnr = bufnr or 0
+    if ensure_empty == nil then
+        ensure_empty = true
+    end
+
+    if ensure_empty then
+        if vim.api.nvim_buf_line_count(bufnr) > 1 then
+            return
+        end
+
+        if #vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] > 0 then
+            return
+        end
+    end
+
     local name, _ = vim.filetype.match({ buf = bufnr })
     if name ~= "cs" then
         error("Boilersharp: You can only write boilerplate on a C# file")
