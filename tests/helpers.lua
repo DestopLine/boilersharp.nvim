8c381a7c526eb496ab3314c60c13a1ee266b101f
DestopLine <destopline555@hotmail.com>
2024-12-21 14:13:57 -0400

    test(helpers): add send() function
    This makes it possible to send variables to a child Neovim. Functions
    are not allowed.

--- tests/helpers.lua
+++ tests/helpers.lua
@@ -3,7 +3,7 @@ local M = {}
 require("mini.test").setup()
 
 ---Makes and initializes a child neovim instance and a test set.
----@param opts table Table to pass to MiniTest.new_set().
+---@param opts table | nil Table to pass to MiniTest.new_set().
 ---@return MiniTest.child
 ---@return table
 function M.new_test(opts)
@@ -13,6 +13,7 @@ function M.new_test(opts)
             pre_case = function()
                 child.restart({ "-u", "scripts/minimal_init.lua" })
                 child.bo.readonly = false
+                child.lua([[M = require("boilersharp")]])
             end,
             post_once = child.stop,
         },
@@ -21,6 +22,15 @@ function M.new_test(opts)
     local set = MiniTest.new_set(set_opts)
 
     return child, set
+end
+
+---Send non-function values into a child Neovim instance
+---@param child MiniTest.child Child Neovim instance
+---@param values { [string]: boolean | string | number| table } Values to send
+function M.send(child, values)
+    for name, value in pairs(values) do
+        child.lua(([[%s = %s]]):format(name, vim.inspect(value)))
+    end
 end
 
 return M
