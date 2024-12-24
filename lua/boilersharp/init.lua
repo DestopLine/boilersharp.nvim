0a3369a4c97742fd27fd7fa56604406b8ab609fb
DestopLine <destopline555@hotmail.com>
2024-12-24 15:31:25 -0400

    refactor(init)!: move core.lua and autocommands.lua code into init.lua
    The functionality in these files was too little and makes more sense
    inside init.lua.

--- lua/boilersharp/init.lua
+++ lua/boilersharp/init.lua
@@ -4,10 +4,42 @@ local M = {}
 function M.setup(opts)
     require("boilersharp.config").init_config(opts)
 
-    -- This require is here to prevent the config from staying
-    -- with the defaults.
-    require("boilersharp.autocommands").add_autocommands()
-    M.write_boilerplate = require("boilersharp.core").write_boilerplate
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+        desc = "Write C# boilerplate when entering an empty C# file",
+        group = vim.api.nvim_create_augroup("Boilersharp", { clear = true }),
+        pattern = "*.cs",
+        callback = M.write_boilerplate,
+    })
+end
+
+---@param bufnr integer | nil
+---@param ensure_empty boolean | nil
+function M.write_boilerplate(bufnr, ensure_empty)
+    bufnr = bufnr or 0
+    if ensure_empty == nil then
+        ensure_empty = true
+    end
+
+    if ensure_empty then
+        if vim.api.nvim_buf_line_count(bufnr) > 1 then
+            return
+        end
+
+        if #vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] > 0 then
+            return
+        end
+    end
+
+    local name, _ = vim.filetype.match({ buf = bufnr })
+    if name ~= "cs" then
+        error("Boilersharp: You can only write boilerplate on a C# file")
+    end
+    local path = vim.api.nvim_buf_get_name(bufnr)
+
+    local boilerplate = require("boilersharp.boilerplate")
+    local boiler = boilerplate.from_file(path)
+    local lines = vim.split(boilerplate.to_string(boiler), "\n")
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
 end
 
 return M
