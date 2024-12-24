6fc2b534623fbf05e35c5b1eca9a93d952b768a9
DestopLine <destopline555@hotmail.com>
2024-12-24 16:03:43 -0400

    refactor(csharp): move types from types.lua to csharp.lua
    Those types are more relevant to csharp.lua. The cache types also are
    only used in that file and for only on variable each. Another reason is
    that other types were already on other files.

--- lua/boilersharp/csharp.lua
+++ lua/boilersharp/csharp.lua
@@ -2,12 +2,42 @@ local config = require("boilersharp.config").config
 
 local M = {}
 
+---@alias boilersharp.AccessModifier
+---| "public"
+---| "protected"
+---| "private"
+---| "internal"
+---| "protected internal"
+---| "private protected"
+---| "file"
+
+---@alias boilersharp.CsharpType
+---| "class"
+---| "struct"
+---| "interface"
+---| "enum"
+---| "record"
+---| "record struct"
+
+---@class boilersharp.DirData
+---@field csproj string | nil Path to csproj file
+---@field namespace string Namespace of the directory
+
+---@class boilersharp.CsprojData
+---@field target_framework string Version of dotnet used 
+---@field cs_version string Version of C# used
+---@field implicit_usings boolean Whether or not the project uses implicit usings
+---@field file_scoped_namespace boolean Whether or not the project supports file scoped namespaces
+---@field root_namespace string | nil The root namespace of the csproj, if applicable
+
 local TSLANG = "xml"
 
----@type boilersharp.DirCache
+---{ [path/to/directory]: Dir }
+---@type { [string]: boilersharp.DirData }
 local dir_cache = {}
 
----@type boilersharp.CsprojCache
+---{ [path/to/project.csproj]: Csproj }
+---@type { [string]: boilersharp.CsprojData }
 local csproj_cache = {}
 
 ---@param path string
