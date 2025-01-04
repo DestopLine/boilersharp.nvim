7c93ae585aad0655c02b8d051eb105a77a0ca242
DestopLine <destopline555@hotmail.com>
2025-01-04 00:18:10 -0400

    style(queries): remove commented code

--- queries/xml/boilersharp.scm
+++ queries/xml/boilersharp.scm
@@ -1,29 +1,3 @@
-; (document
-;   root: (element
-;     (STag
-;       (Name) @name.project (#eq? @name.project "Project"))
-;     (content
-;       (element
-;         (STag
-;           (Name) @name.pgroup (#eq? @name.pgroup "PropertyGroup"))
-;         (content
-;           (element
-;             (STag
-;               (Name) @name.tframe (#eq? @name.tframe "TargetFramework"))
-;             (content 
-;               (CharData) @dotnet_version))
-;           (element
-;             (STag
-;               (Name) @name.iusing (#eq? @name.iusing "ImplicitUsings"))
-;             (content
-;               (CharData) @implicit_usings))
-;           (element
-;             (STag
-;               (Name) @name.langver (#eq? @name.langver "LangVersion"))
-;             (content
-;               (CharData) @lang_version)))))))
-
-
 (document
   root: (element
     (STag
